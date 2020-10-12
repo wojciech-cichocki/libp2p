05efc971e175a34e2af47c87aa8c737b5b0c207c
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-13 00:47:27 +0200

    #1 Add joinTopic function

--- src/pub-sub.js
+++ src/pub-sub.js
@@ -7,15 +7,28 @@ class PubSub {
 
         this.libp2p.connectionManager.on('peer:connect', this.handleConnect.bind(this))
         this.libp2p.connectionManager.on('peer:disconnect', this.handleDisconnect.bind(this))
+
+        if (this.libp2p.isStarted()) this.joinTopic()
     }
 
     handleConnect(connection) {
+        console.info(`Connected to ${connection.remotePeer.toB58String()}`)
         if (this.connectedPeers.has(connection.remotePeer.toB58String())) return
         this.connectedPeers.add(connection.remotePeer.toB58String())
     }
 
     handleDisconnect(connection) {
+        console.info(`Disconnect ${connection.remotePeer.toB58String()}`)
         this.connectedPeers.delete(connection.remotePeer.toB58String())
+    }
+
+    onStart() {
+        this.joinTopic()
+    }
+
+    joinTopic() {
+        this.libp2p.pubsub.on(this.topic, () => console.log('on message'))
+        this.libp2p.pubsub.subscribe(this.topic)
     }
 }
 
