d4920ce38130f05bc0977564a8a5c8943fd6a345
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-13 19:43:57 +0200

    #3 Add 'should decode Seat' test

--- src/protocol-payload.spec.js
+++ src/protocol-payload.spec.js
@@ -1,27 +1,33 @@
 const uint8arrayFromString = require('uint8arrays/from-string')
+const uint8arrayToString = require('uint8arrays/to-string')
 const assert = require('assert')
 
 const {SeatsUpdate, Seat} = require('./protocol-payload')
 
 describe('Protocol Payload Test', () => {
     context('First test case', () => {
-        const id = 1;
-        const type = Seat.Type.FREE;
-        const peerId = 'QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d'
-        const created = Date.now()
+        const expectedId = 1;
+        const expectedType = Seat.Type.FREE;
+        const expectedPeerId = 'QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d'
+        const expectedCreated = Date.now()
         let seatPayload
 
         before('should encode Seat', () => {
             seatPayload = Seat.encode({
-                id,
-                type,
-                peerId: uint8arrayFromString(peerId),
-                created
+                id: expectedId,
+                type: expectedType,
+                peerId: uint8arrayFromString(expectedPeerId),
+                created: expectedCreated
             })
         })
 
-        it('ok' ,  () => {
-            assert.ok(true)
+        it('should decode Seat' ,  () => {
+            const {id, type, peerId, created } = Seat.decode(seatPayload);
+
+            assert.strictEqual(expectedId, id)
+            assert.strictEqual(expectedType, type)
+            assert.strictEqual(expectedPeerId, uint8arrayToString(peerId))
+            assert.strictEqual(expectedCreated, created)
         })
     })
 })
