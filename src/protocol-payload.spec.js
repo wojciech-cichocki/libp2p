942f8a325d1a652ac79415b343580c15bad66c37
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-13 19:27:13 +0200

    #3 Move test case to separate context

--- src/protocol-payload.spec.js
+++ src/protocol-payload.spec.js
@@ -4,18 +4,24 @@ const assert = require('assert')
 const {SeatsUpdate, Seat} = require('./protocol-payload')
 
 describe('Protocol Payload Test', () => {
-    let seatPayload
+    context('First test case', () => {
+        const id = 1;
+        const type = Seat.Type.FREE;
+        const peerId = 'QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d'
+        const created = Date.now()
+        let seatPayload
 
-    before('should encode Seat', () => {
-        seatPayload = Seat.encode({
-            id: 1,
-            type: Seat.Type.FREE,
-            peerId: uint8arrayFromString('QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d'),
-            created: Date.now()
+        before('should encode Seat', () => {
+            seatPayload = Seat.encode({
+                id: id,
+                type: type,
+                peerId: uint8arrayFromString(peerId),
+                created: created
+            })
         })
-    })
 
-    it('ok' ,  () => {
-        assert.ok(true)
+        it('ok' ,  () => {
+            assert.ok(true)
+        })
     })
 })
