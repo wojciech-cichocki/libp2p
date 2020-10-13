008892e86728d7ff64430c1e6671a2d238975417
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-13 23:43:29 +0200

    #3 Add encode/decode payload test to free and taken seats

--- src/protocol-payload.spec.js
+++ src/protocol-payload.spec.js
@@ -5,9 +5,11 @@ const assert = require('assert')
 const {SeatsUpdate, Seat} = require('./protocol-payload')
 
 describe('Protocol Payload Test', () => {
-    context('First test case', () => {
+    let expectedSeatsUpdate = []
+
+    context('Taken seat test case', () => {
         const expectedId = 1;
-        const expectedType = Seat.Type.FREE;
+        const expectedType = Seat.Type.TAKEN;
         const expectedPeerId = 'QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d'
         const expectedCreated = Date.now()
         let seatPayload
@@ -19,6 +21,7 @@ describe('Protocol Payload Test', () => {
                 peerId: uint8arrayFromString(expectedPeerId),
                 created: expectedCreated
             })
+            expectedSeatsUpdate.push(seatPayload)
         })
 
         it('should decode Seat' ,  () => {
@@ -27,6 +30,30 @@ describe('Protocol Payload Test', () => {
             assert.strictEqual(expectedId, id)
             assert.strictEqual(expectedType, type)
             assert.strictEqual(expectedPeerId, uint8arrayToString(peerId))
+            assert.strictEqual(expectedCreated, created)
+        })
+    })
+
+    context('Free seat test case', () => {
+        const expectedId = 2;
+        const expectedType = Seat.Type.FREE;
+        const expectedCreated = Date.now()
+        let seatPayload
+
+        before('should encode Seat', () => {
+            seatPayload = Seat.encode({
+                id: expectedId,
+                type: expectedType,
+                created: expectedCreated
+            })
+            expectedSeatsUpdate.push(seatPayload)
+        })
+
+        it('should decode Seat' ,  () => {
+            const {id, type, created } = Seat.decode(seatPayload);
+
+            assert.strictEqual(expectedId, id)
+            assert.strictEqual(expectedType, type)
             assert.strictEqual(expectedCreated, created)
         })
     })
