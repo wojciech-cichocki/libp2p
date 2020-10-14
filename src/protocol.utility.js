0dc1634aac5506319a6223ba16df1ea99519ed64
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-14 03:51:35 +0200

    #3 Add encode/decode seat utility

--- src/protocol.utility.js
+++ src/protocol.utility.js
@@ -1,6 +1,23 @@
 const uint8arrayFromString = require('uint8arrays/from-string')
 const uint8arrayToString = require('uint8arrays/to-string')
 
+const {SeatsUpdate, Seat} = require('./protocol.model')
+
+const encodeSeat = ({id, type, peerId, created}) => {
+    return Seat.encode(seatToBytes(
+        {
+            id,
+            type,
+            peerId,
+            created
+        }
+    ))
+}
+
+const decodeSeat = (encodedSeat) => {
+    return seatFromBytes(Seat.decode(encodedSeat))
+}
+
 const seatToBytes = (seat) => {
     return transformMsg(seat, uint8arrayFromString)
 }
@@ -19,6 +36,6 @@ const transformMsg = (seat, peerIdConverter) => {
 }
 
 module.exports = {
-    seatToBytes,
-    seatFromBytes
+    encodeSeat,
+    decodeSeat,
 }
