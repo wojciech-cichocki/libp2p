ae0b4d076333264c09e5dbb1d18b3c026bb881c3
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-19 01:15:12 +0200

    #11 Add init node in app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { ThemeProvider } from '@material-ui/core'
-import { theme } from './static/theme'
-import { MainPage } from './containers/MainPage/MainPage'
+import React, {useEffect, useState} from 'react'
+import {ThemeProvider} from '@material-ui/core'
+import {theme} from './static/theme'
+import {MainPage} from './containers/MainPage/MainPage'
+import {getOrCreatePeerId} from "./p2p-node/peerId";
+import {createNode} from "./p2p-node/libp2p";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <MainPage></MainPage>
-    </ThemeProvider>
-  )
+    const [peerId, setPeerId] = useState()
+    const [libp2p, setLibp2p] = useState()
+    const [initialized, setInitialized] = useState(false)
+
+    const initLibp2p = async () => {
+        const node = await createNode(peerId);
+        await node.start()
+
+        setLibp2p(node)
+        setInitialized(true)
+    }
+
+    useEffect(() => {
+        if (!peerId) {
+            getOrCreatePeerId().then(setPeerId)
+            return
+        }
+
+
+        if (!libp2p) {
+            initLibp2p()
+        }
+    })
+
+    return (
+        <ThemeProvider theme={theme}>
+            <MainPage></MainPage>
+            <div>{peerId ? peerId.id : ('not s')}</div>
+        </ThemeProvider>
+    )
 }
 
 export default App
