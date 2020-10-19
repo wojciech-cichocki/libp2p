39288bf9ac1d2c005a05404b062c15d7327fc42c
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-19 02:07:10 +0200

    #11 Integrate protocol into app

---
+++ app/src/protocol/protocol.handler.js
@@ -0,0 +1,85 @@
+const {
+    decodeMessage, decodeReleaseSeatRequest, decodeTakeSeatRequest, encodeCurrentState,
+    decodeCurrentState, getLastUpdateTimestamp, checkSeatIsFree, checkSeatIsTakenByPeer
+} = require('./protocol.utility')
+const {Message, Seat} = require('./protocol.model')
+
+const connectionHandler = (connection) => {
+    console.info(`Connected to ${connection.remotePeer.toB58String()}`)
+}
+
+const receiveMessageHandler = ({from, data}, pubSub, state) => {
+    const peerId = pubSub.libp2p.peerId._idB58String
+    const message = decodeMessage(data);
+
+    switch (message.type) {
+        case Message.Type.REQUIRES_SYNCHRONIZATION: {
+            if (peerId === from) {
+                return state
+            }
+
+            if (state.init) {
+                pubSub.send(encodeCurrentState(state.firstSeat, state.secondSeat))
+            }
+            break
+        }
+        case Message.Type.CURRENT_STATE: {
+            if (peerId === from) {
+                return state
+            }
+            const message = decodeCurrentState(data)
+            const currentTimestamp = getLastUpdateTimestamp(state);
+            const receivedTimestamp = getLastUpdateTimestamp(message);
+
+            if (currentTimestamp === null || receivedTimestamp > currentTimestamp) {
+                const {firstSeat, secondSeat} = message
+                state = {firstSeat, secondSeat, init: true}
+            }
+            break
+        }
+        case Message.Type.TAKE_SEAT_REQUEST: {
+            const {id, timestamp} = decodeTakeSeatRequest(data)
+            const firstSeat = state.firstSeat
+            const secondSeat = state.secondSeat
+
+            if (id === firstSeat.id && checkSeatIsFree(state.firstSeat)) {
+                takeSeat(state.firstSeat, from, timestamp)
+
+            } else if (id === secondSeat.id && checkSeatIsFree(state.secondSeat)) {
+                takeSeat(state.secondSeat, from, timestamp)
+            }
+            break
+        }
+        case Message.Type.RELEASE_SEAT_REQUEST: {
+            const {id, timestamp} = decodeReleaseSeatRequest(data)
+            const firstSeat = state.firstSeat
+            const secondSeat = state.secondSeat
+
+            if (id === firstSeat.id && checkSeatIsTakenByPeer(state.firstSeat, from)) {
+                releaseSeat(state.firstSeat, timestamp)
+
+            } else if (id === secondSeat.id && checkSeatIsTakenByPeer(state.secondSeat, from)) {
+                releaseSeat(state.secondSeat, timestamp)
+            }
+            break
+        }
+    }
+    return state
+}
+
+const takeSeat = (seat, peerId, timestamp) => {
+    seat.timestamp = timestamp
+    seat.type = Seat.Type.TAKEN
+    seat.peerId = peerId
+}
+
+const releaseSeat = (seat, timestamp) => {
+    seat.timestamp = timestamp
+    seat.type = Seat.Type.FREE
+    delete seat.peerId
+}
+
+module.exports = {
+    connectionHandler,
+    receiveMessageHandler
+}
---
+++ app/src/protocol/protocol.utility.js
@@ -0,0 +1,143 @@
+const uint8arrayFromString = require('uint8arrays/from-string')
+const uint8arrayToString = require('uint8arrays/to-string')
+
+const {Seat, Message} = require('./protocol.model')
+
+
+const encodeRequiresSynchronization = () => {
+    return Message.encode({
+        type: Message.Type.REQUIRES_SYNCHRONIZATION
+    })
+}
+
+const decodeMessage = (encodedMessage) => {
+    return Message.decode(encodedMessage)
+}
+
+const encodeSeat = ({id, type, peerId, timestamp}) => {
+    return Seat.encode(seatToBytes(
+        {
+            id,
+            type,
+            peerId,
+            timestamp
+        }
+    ))
+}
+
+const decodeSeat = (encodedSeat) => {
+    return seatFromBytes(Seat.decode(encodedSeat))
+}
+
+const encodeTakeSeatRequest = ({id, timestamp}) => {
+    return Message.encode({
+        type: Message.Type.TAKE_SEAT_REQUEST,
+        takeSeatRequest: {
+            id,
+            timestamp
+        }
+    })
+}
+
+const decodeTakeSeatRequest = (encodedTakeSeatRequest) => {
+    const {takeSeatRequest} = Message.decode(encodedTakeSeatRequest)
+    return takeSeatRequest
+}
+
+const encodeReleaseSeatRequest = ({id, timestamp}) => {
+    return Message.encode({
+        type: Message.Type.RELEASE_SEAT_REQUEST,
+        releaseSeatRequest: {
+            id,
+            timestamp
+        }
+    })
+}
+
+const decodeReleaseSeatRequest = (encodedReleaseSeatRequest) => {
+    const {releaseSeatRequest} = Message.decode(encodedReleaseSeatRequest)
+    return releaseSeatRequest
+}
+
+const encodeCurrentState = (firstSeat, secondSeat) => {
+    return Message.encode({
+        type: Message.Type.CURRENT_STATE,
+        currentState: {
+            firstSeat: seatToBytes(firstSeat),
+            secondSeat: seatToBytes(secondSeat)
+        }
+    })
+}
+
+const decodeCurrentState = (encodedCurrentState) => {
+    const {currentState: decodeCurrentState} = Message.decode(encodedCurrentState)
+
+    return {
+        firstSeat: seatFromBytes(decodeCurrentState.firstSeat),
+        secondSeat: seatFromBytes(decodeCurrentState.secondSeat)
+    }
+}
+
+const seatToBytes = (seat) => {
+    return transformMsg(seat, uint8arrayFromString)
+}
+
+const seatFromBytes = (seat) => {
+    return transformMsg(seat, uint8arrayToString)
+}
+
+const transformMsg = (seat, peerIdConverter) => {
+    const output = Object.assign({}, seat);
+    if (output.peerId) {
+        return Object.assign(output, {peerId: peerIdConverter(output.peerId)})
+    }
+    delete output.peerId
+    return output
+}
+
+const getLastUpdateTimestamp = (currentState) => {
+    const isFirstSeat = !!currentState.firstSeat
+    const isSecondSeat = !!currentState.secondSeat
+
+    if (!(isFirstSeat || isSecondSeat)) {
+        return null
+    }
+
+    const firstTimestamp = currentState.firstSeat.timestamp;
+    const secondTimestamp = currentState.secondSeat.timestamp;
+    const now = Date.now()
+
+    if (isFirstSeat && !isSecondSeat) {
+        return firstTimestamp
+    } else if (isSecondSeat && !isFirstSeat) {
+        return secondTimestamp
+    } else if (firstTimestamp > now || secondTimestamp > now) {
+        return null
+    }
+
+    return firstTimestamp > secondTimestamp ? firstTimestamp : secondTimestamp
+}
+
+const checkSeatIsFree = (seat) => {
+    return seat.type === Seat.Type.FREE
+}
+
+const checkSeatIsTakenByPeer = (seat, peerId) => {
+    return seat.type === Seat.Type.TAKEN && seat.peerId === peerId
+}
+
+module.exports = {
+    encodeSeat,
+    decodeSeat,
+    encodeCurrentState,
+    decodeCurrentState,
+    encodeTakeSeatRequest,
+    decodeTakeSeatRequest,
+    encodeReleaseSeatRequest,
+    decodeReleaseSeatRequest,
+    decodeMessage,
+    getLastUpdateTimestamp,
+    checkSeatIsFree,
+    checkSeatIsTakenByPeer,
+    encodeRequiresSynchronization
+}
