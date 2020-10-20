29b8e182299186817eafe6d4152866465222a775
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-20 19:17:38 +0200

    #11 Remove parcel PoC

--- parcel-poc/index.js
+++
@@ -1,7 +0,0 @@
-import 'regenerator-runtime/runtime.js'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import createLibp2p from "./libp2p/libp2p";
-
-ReactDOM.render(<App createLibp2p={createLibp2p}/>, document.getElementById('root'))
