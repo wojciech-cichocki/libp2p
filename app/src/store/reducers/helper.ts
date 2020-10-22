df790ca9f2c26e4b8eebaa0e8d810d37d731105c
wojciech-cichocki <cichockiwojciech97@gmail.com>
2020-10-22 04:10:18 +0200

    #17 Add reducer helper

---
+++ app/src/store/reducers/helper.ts
@@ -0,0 +1,42 @@
+import {Seat, SeatRequest, SeatType} from "../types";
+
+export const takeSeat = (seat: Seat, seatRequest: SeatRequest): Seat => {
+    return {
+        id: seat.id,
+        peerId: seatRequest.from,
+        type: SeatType.TAKEN,
+        timestamp: seatRequest.timestamp
+    }
+}
+
+export const checkSeatIsFree = (id: number, seat?: Seat | undefined) => {
+    if(seat === undefined) {
+        return true
+    }
+
+    if(id !== seat?.id) {
+        return false
+    }
+
+    return seat.type === SeatType.FREE
+}
+
+export const checkSeatIsTaken = (seat: Seat, from: string) => {
+    return seat.type === SeatType.TAKEN && seat.peerId === from
+}
+
+export const getLastTimestamp = (t1: number | undefined, t2: number | undefined): number | undefined => {
+    if (t1 === undefined && t2 === undefined) {
+        return undefined
+    }
+
+    if (t1 !== undefined && t2 === undefined) {
+        return t1
+    }
+
+    if (t1 === undefined && t2 !== undefined) {
+        return t2
+    }
+
+    return (t1 as number) >= (t2 as number) ? t1 : t2
+}
