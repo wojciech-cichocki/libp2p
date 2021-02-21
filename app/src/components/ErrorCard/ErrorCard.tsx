a059c0eef97008b4e34ae6485b1302304eddb876
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-02-21 13:59:33 +0100

    #24 Move wrapper from ErrorCard to CriticalAppError

--- app/src/components/ErrorCard/ErrorCard.tsx
+++ app/src/components/ErrorCard/ErrorCard.tsx
@@ -2,7 +2,6 @@ import {Grid} from "@material-ui/core";
 import React from "react";
 
 import useStyles from './style'
-import FullHeight from "../../wrappers/full-height/FullHeight";
 
 export interface IErrorCard {
     title: string,
@@ -13,13 +12,9 @@ export const ErrorCard: React.FC<IErrorCard> = ({title, message}) => {
     const classes = useStyles();
 
     return (
-        <FullHeight verticalAlign={true} horizontalAlign={true}>
-            <Grid container alignItems="center" justify="center">
-                <Grid className={classes.root}>
-                    <h2>{title}</h2>
-                    <h4>{message}</h4>
-                </Grid>
-            </Grid>
-        </FullHeight>
+        <Grid className={classes.root}>
+            <h2>{title}</h2>
+            <h4>{message}</h4>
+        </Grid>
     )
 }
--- app/src/wrappers/error-boundary/CriticalAppError.tsx
+++ app/src/wrappers/error-boundary/CriticalAppError.tsx
@@ -1,6 +1,8 @@
 import React, {Fragment, ReactElement, ReactNode} from 'react';
 import Catch from './ErrorBoundaryUtility';
 import {ErrorCard} from "../../components/ErrorCard/ErrorCard";
+import FullHeight from "../full-height/FullHeight";
+import {Grid} from "@material-ui/core";
 
 interface Props {
     children: ReactNode;
@@ -10,7 +12,11 @@ const CriticalAppError = Catch(
     ({children}: Props, error?: Error): ReactElement<any, any> => {
         if (error) {
             return (
-                <ErrorCard title="An error has occurred in the application" message={error.message}/>
+                <FullHeight verticalAlign={true} horizontalAlign={true}>
+                    <Grid container alignItems="center" justify="center">
+                        <ErrorCard title="An error has occurred in the application" message={error.message}/>
+                    </Grid>
+                </FullHeight>
             )
         } else {
             return <Fragment>{children}</Fragment>;
