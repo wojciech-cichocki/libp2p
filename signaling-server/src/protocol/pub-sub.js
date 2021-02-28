a61be0b88d0f7628b3417851087d9142aea2e90f
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-02-28 14:18:11 +0100

    #39 Fix initial requires synchronization bug

--- signaling-server/src/protocol/pub-sub.js
+++ signaling-server/src/protocol/pub-sub.js
@@ -1,82 +1,99 @@
-const {connectionHandler, receiveMessageHandler} = require('./protocol.handler')
-const {encodeRequiresSynchronization, encodeTakeSeatRequest, encodeReleaseSeatRequest} = require('./protocol.utility')
+const {
+  connectionHandler,
+  receiveMessageHandler,
+} = require("./protocol.handler");
+const {
+  encodeRequiresSynchronization,
+  encodeTakeSeatRequest,
+  encodeReleaseSeatRequest,
+} = require("./protocol.utility");
 
 class PubSub {
-    constructor(libp2p, topic, state) {
-        this.libp2p = libp2p
-        this.TOPIC = topic
-        this.state = state
-        this.connectionHandler = connectionHandler
-        this.receiveMessageHandler = receiveMessageHandler
-        this.connectedPeers = new Set()
+  constructor(libp2p, topic, state) {
+    this.libp2p = libp2p;
+    this.topic = topic;
+    this.state = state;
+    this.connectionHandler = connectionHandler;
+    this.receiveMessageHandler = receiveMessageHandler;
+    this.connectedPeers = new Set();
 
-        this.libp2p.connectionManager.on('peer:connect', this.handleConnect.bind(this))
-        this.libp2p.connectionManager.on('peer:disconnect', this.handleDisconnect.bind(this))
-        this._onMessage = this._onMessage.bind(this)
+    this.libp2p.connectionManager.on(
+      "peer:connect",
+      this.handleConnect.bind(this)
+    );
+    this.libp2p.connectionManager.on(
+      "peer:disconnect",
+      this.handleDisconnect.bind(this)
+    );
+    this._onMessage = this._onMessage.bind(this);
 
-        if (this.libp2p.isStarted()) this.joinTopic()
+    if (this.libp2p.isStarted()) this.joinTopic();
+  }
+
+  handleConnect(connection) {
+    if (this.connectedPeers.has(connection.remotePeer.toB58String())) return;
+    this.connectedPeers.add(connection.remotePeer.toB58String());
+
+    if (this.connectionHandler) {
+      this.connectionHandler(connection);
     }
+  }
 
-    handleConnect(connection) {
-        if (this.connectedPeers.has(connection.remotePeer.toB58String())) return
-        this.connectedPeers.add(connection.remotePeer.toB58String())
+  handleDisconnect(connection) {
+    console.info(`Disconnect ${connection.remotePeer.toB58String()}`);
+    this.connectedPeers.delete(connection.remotePeer.toB58String());
+  }
 
-        if (this.connectionHandler) {
-            this.connectionHandler(connection)
-        }
-    }
+  onStart() {
+    this.joinTopic();
+  }
 
-    handleDisconnect(connection) {
-        console.info(`Disconnect ${connection.remotePeer.toB58String()}`)
-        this.connectedPeers.delete(connection.remotePeer.toB58String())
-    }
+  onStop() {
+    this.leaveTopic();
+  }
 
-    onStart() {
-        this.joinTopic()
-    }
+  joinTopic() {
+    this.libp2p.pubsub.on(this.topic, this._onMessage);
+    this.libp2p.pubsub.subscribe(this.topic);
+  }
 
-    onStop() {
-        this.leaveTopic()
-    }
+  leaveTopic() {
+    this.libp2p.pubsub.removeListener(this.topic, this._onMessage);
+    this.libp2p.pubsub.unsubscribe(this.topic);
+  }
 
-    joinTopic() {
-        this.libp2p.pubsub.on(this.topic, this._onMessage)
-        this.libp2p.pubsub.subscribe(this.topic)
-    }
+  _onMessage(message) {
+    const newState = this.receiveMessageHandler(message, this, this.state);
+    this.state = newState;
+  }
 
-    leaveTopic() {
-        this.libp2p.pubsub.removeListener(this.topic, this._onMessage)
-        this.libp2p.pubsub.unsubscribe(this.topic)
-    }
+  async send(msg) {
+    await this.libp2p.pubsub.publish(this.topic, msg);
+  }
 
-    _onMessage(message) {
-        const newState = this.receiveMessageHandler(message, this, this.state);
-        this.state = newState
-    }
+  requiresSynchronization() {
+    setTimeout(() => {
+      this.send(encodeRequiresSynchronization());
+    }, 1000);
+  }
 
-    async send(msg) {
-        await this.libp2p.pubsub.publish(this.topic, msg)
-    }
+  takeSeat(id) {
+    this.send(
+      encodeTakeSeatRequest({
+        id: id,
+        timestamp: Date.now(),
+      })
+    );
+  }
 
-    requiresSynchronization() {
-        setTimeout(() => {
-            this.send(encodeRequiresSynchronization())
-        }, 1000)
-    }
-
-    takeSeat(id) {
-        this.send(encodeTakeSeatRequest({
-            id: id,
-            timestamp: Date.now()
-        }))
-    }
-
-    releaseSeat(id) {
-        this.send(encodeReleaseSeatRequest({
-            id: id,
-            timestamp: Date.now()
-        }))
-    }
+  releaseSeat(id) {
+    this.send(
+      encodeReleaseSeatRequest({
+        id: id,
+        timestamp: Date.now(),
+      })
+    );
+  }
 }
 
-module.exports = PubSub
+module.exports = PubSub;
